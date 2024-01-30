b71c49cef157fac0fcbc0e37e471e2d40620de5b
mininhong <in000824@naver.com>
2024-01-30 09:43:30 +0900

    20240130 commit

--- ex_8/script.js
+++ ex_8/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const parentDiv = document.getElementById("parent").children;
 
 for (const childDiv of parentDiv) {
@@ -6,12 +5,3 @@ for (const childDiv of parentDiv) {
     e.target.remove();
   });
 }
-=======
-const parentDiv = document.getElementById("parent").children;
-
-for (const childDiv of parentDiv) {
-  childDiv.addEventListener("click", (e) => {
-    e.target.remove();
-  });
-}
->>>>>>> 3aa5570b7aab8c87944effcadb136c2ce003cfd2
